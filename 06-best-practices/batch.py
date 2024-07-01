acd29522cb6c3f909704b065b62296920185cade
alex-sokolov2011 <sokaa2011@gmail.com>
2024-07-01 12:03:08 +0300

    add solution for Q1

--- 06-best-practices/batch.py
+++ 06-best-practices/batch.py
@@ -1,52 +1,68 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import warnings
 import sys
 import pickle
 import pandas as pd
+import click
+
+warnings.filterwarnings("ignore", category=UserWarning, module='sklearn')
 
 
-year = int(sys.argv[1])
-month = int(sys.argv[2])
-
-input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+def generate_output_file_path(year, month):
+ return f'./output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
 
 
-with open('model.bin', 'rb') as f_in:
-    dv, lr = pickle.load(f_in)
-
-
-categorical = ['PULocationID', 'DOLocationID']
-
-def read_data(filename):
+def read_data(filename, categorical):
     df = pd.read_parquet(filename)
-    
+
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
 
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-    
-    return df
+
+    return df, categorical
+
+@click.command()
+@click.option(
+    '--year',
+    type=int,
+    required=True,
+    help='Year of the trip data'
+)
+@click.option(
+    '--month',
+    type=int,
+    required=True,
+    help='Month of the trip data'
+)
+def main(year, month):
+    with open('./description_homework/model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
+
+    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    df, categorical = read_data(input_file, categorical = ['PULocationID', 'DOLocationID'])
+
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+
+    print('predicted mean duration:', y_pred.mean())
 
 
-df = read_data(input_file)
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+
+    output_file = generate_output_file_path(year, month)
+    df_result.to_parquet(output_file, engine='pyarrow', index=False)
 
 
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
+if __name__ == '__main__':
+    main()
 
-
-print('predicted mean duration:', y_pred.mean())
-
-
-df_result = pd.DataFrame()
-df_result['ride_id'] = df['ride_id']
-df_result['predicted_duration'] = y_pred
-
-
-df_result.to_parquet(output_file, engine='pyarrow', index=False)
